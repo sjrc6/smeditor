02c78f1b13d16ab1ab4eb4f7fd43b8215cb84eb2
Tater <Mr.Potatooh@gmail.com>
2026-02-18 22:59:57 -0600

    fix build attempt

--- nwbuild.js
+++ nwbuild.js
@@ -1,33 +1,16 @@
 import nwbuild from "nw-builder";
 
 
+const nwjsVersion = process.env.NWJS_VERSION ?? "0.90.0"
+
 const baseOptions = {
   srcDir: "./nw",
   mode: "build",
-  version: "latest",
+  version: nwjsVersion,
   glob: false,
   logLevel: "debug",
   arch: "x64",
 }
-
-const mac_x64 = Object.assign(structuredClone(baseOptions), {
-  outDir: "build/mac-x64/SMEditor",
-  platform: "osx",
-  app: {
-    name: "SMEditor",
-    genericName: "SMEditor",
-    icon: "./public/assets/icon/mac.icns",
-    comment: "Open source tool to view and edit StepMania charts",
-    CFBundleIdentifier: "io.github.tillvit.smeditor",
-    NSHumanReadableCopyright: ""
-  }
-})
-
-const mac_arm = Object.assign(structuredClone(mac_x64), {
-  outDir: "build/mac-arm/SMEditor",
-  arch: "arm64",
-})
-
 
 const win = Object.assign(structuredClone(baseOptions), {
   outDir: "build/win/SMEditor",
@@ -43,18 +26,5 @@ const win = Object.assign(structuredClone(baseOptions), {
 })
 
 
-const linux = Object.assign(structuredClone(baseOptions), {
-  outDir: "build/linux/SMEditor",
-  platform: "linux",
-  app: {
-    name: "SMEditor",
-    genericName: "SMEditor",
-    icon: "./public/assets/icon/favicon.ico",
-    comment: "Open source tool to view and edit StepMania charts"
-  }
-})
-
-await nwbuild(mac_arm)
-await nwbuild(mac_x64)
+console.log(`Building desktop target: win`)
 await nwbuild(win)
-await nwbuild(linux)
